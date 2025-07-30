adde50bc0d2513217263be409f2dd8ab5f0361b5
Michalis Goumenakis <m.goumenakis@i-docs.com>
2025-07-30 11:47:33 +0300

    setup supabase

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -9,10 +9,26 @@ export default function AppProtectedLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="chat" />
+      <Stack.Screen name="+not-found" />
+      {/* Modal screen */}
+      <Stack.Screen
+        name="(modals)/slot/[id]"
+        options={{
+          presentation: "modal",
+          headerShown: false, // or false
+        }}
+      />
+      <Stack.Screen
+        name="(modals)/slot/manage/[id]"
+        options={{
+          presentation: "modal",
+          headerShown: true,
+          headerTitle: "Manage Slot",
+        }}
+      />
+    </Stack>
   );
 }
